64d903633a4c5f0ee89a63950fd7e51e96ee1542
jaguilar <julio.aguilar@lania.edu.mx>
2023-01-09 21:04:07 -0600

    Quita import no usado

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -1,6 +1,5 @@
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
